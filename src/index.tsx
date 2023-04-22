d6603a97e5914561a306ceddfb9a3b5125254ddc
P1X3L_R4 <p1x3lr4@gmail.com>
2023-04-22 11:59:52 -0300

    Criação do titulo do app, criação do campo 'adicionar', criação do botão adicionar

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
-import Font from 'react-native-vector-icons/FontAwesome';
+import {Text, TextInput, View, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-function App(): JSX.Element {
+export default function App() {
   return (
-    <View className="flex-1 items-center justify-center px-10 bg-zinc-900">
-      <Text className="text-zinc-200">APP</Text>
-      <Font color={'#1919'} name="card" size={48} />
+    <View className="flex-1 bg-zinc-900 p-4">
+      <View className="flex flex-col ">
+        <Text
+          className="text-2xl text-zinc-200 font-mono font-extrabold"
+          key={1}>
+          Nome do Evento
+        </Text>
+        <Text className="text-zinc-500 text-base" key={2}>
+          Sexta, 4 de Novembro de 2022
+        </Text>
+      </View>
+
+      <View className="flex-row items-center justify-center gap-2 mt-8">
+        <TextInput className="bg-zinc-800 rounded flex-1 p-4 text-zinc-50" />
+        <TouchableOpacity className="p-4 bg-green-500 rounded">
+          <Icon name="plus" color={'rgb(228,228,231)'} size={24} />
+        </TouchableOpacity>
+      </View>
+
+      
     </View>
   );
 }
-
-export default App;
