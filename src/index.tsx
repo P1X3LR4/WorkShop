11844060d36d6bf436f5479b4b707f71b91a4e86
P1X3L_R4 <p1x3lr4@gmail.com>
2023-05-01 23:55:42 -0300

    name default

--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,24 @@ import Icon from 'react-native-vector-icons/Feather';
 
 export default function App() {
   return (
-    <View className="flex-1 bg-zinc-900 p-4">
-      <View className="flex flex-col ">
-        <Text
-          className="text-2xl text-zinc-200 font-mono font-extrabold"
-          key={1}>
-          Nome do Evento
-        </Text>
-        <Text className="text-zinc-500 text-base" key={2}>
-          Sexta, 4 de Novembro de 2022
-        </Text>
-      </View>
+      <View className="flex-1 bg-zinc-900/100 p-4">
+        <View className="flex flex-col ">
+          <Text
+            className="text-2xl text-zinc-200 font-mono font-extrabold"
+            key={1}>
+            Nome do Evento
+          </Text>
+          <Text className="text-zinc-500 text-base" key={2}>
+            Sexta, 4 de Novembro de 2022
+          </Text>
+        </View>
 
-      <View className="flex-row items-center justify-center gap-2 mt-8">
-        <TextInput className="bg-zinc-800 rounded flex-1 p-4 text-zinc-50" />
-        <TouchableOpacity className="p-4 bg-green-500 rounded">
-          <Icon name="plus" color={'rgb(228,228,231)'} size={24} />
-        </TouchableOpacity>
+        <View className="flex-row items-center justify-center gap-2 mt-8">
+          <TextInput className="bg-zinc-800 rounded flex-1 p-4 text-zinc-50 text-base" />
+          <TouchableOpacity className="p-4 bg-green-500 rounded">
+            <Icon name="plus" color={'rgb(228,228,231)'} size={24} />
+          </TouchableOpacity>
+        </View>
       </View>
-
-      
-    </View>
   );
 }
